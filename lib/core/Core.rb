d29c71993698330e78629913ee83368c60866c5f
Andrew Kirschke <amkirsch@us.ibm.com>
2016-06-03 11:46:19 -0400

    switched to 'ruby-git' gem; added 'Core' module and some methods

---
+++ lib/core/Core.rb
@@ -0,0 +1,27 @@
+require 'git'
+require 'issue'
+
+module Core
+
+  module Repo
+    extend self
+
+    def open(working_dir = nil)
+      repo = Git.open(working_dir)
+    end
+
+    def assets(working_dir = '.')
+      repo = open(working_dir)
+      lib = Git::Lib.new(repo)
+      currentBranch = lib.branch_current
+      lib.checkout('issue')
+      puts Issue.all
+      lib.checkout(currentBranch)
+    end
+
+  end
+end
+
+include Core::Repo
+
+Core::Repo.assets
