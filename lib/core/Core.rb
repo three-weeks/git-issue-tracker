8f0a6baf26252688f26952f68c5d2c01e6d267ea
Andrew Kirschke <andrew.kirschke@gmx.com>
2016-06-04 22:47:55 -0400

    Extended Stash to have 'stash_pop' method, until ruby-git gem is updated to support natively.

--- lib/core/Core.rb
+++ lib/core/Core.rb
@@ -2,6 +2,16 @@ require 'git'
 require_relative "Issue"
 require_relative "Comment"
 
+module AdditionalMethods
+  def stash_pop(id=nil)
+    if id
+      command('stash pop', [id])
+    else
+      command('stash pop')
+    end
+  end
+end
+
 module Core
 
   module Repo
@@ -14,6 +24,7 @@ module Core
     def assets(working_dir = '.')
 
       repo = open(working_dir)
+      repo.lib.extend(AdditionalMethods)
 
       currentBranch = repo.lib.branch_current # keep branch, so we can switch back
       if repo.status.changed == {}
@@ -34,7 +45,7 @@ module Core
       puts Comment.all
 
       repo.lib.checkout(currentBranch)
-      repo.lib.stash_apply('stash@{0}')
+      repo.lib.stash_pop('stash@{0}')
     end
 
   end
